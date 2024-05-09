efb63deaf6ac1ada6e99e63d9157664fdecf177b
vuongle-dev <vuongle.dev@gmail.com>
2024-05-09 12:46:08 +0700

    fix package.json

--- src/config/axiosClient.tsx
+++ src/config/axiosClient.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClient = axios.create({
-  baseURL: "http://localhost:9000/",
+  baseURL: process.env.NEXT_PUBLIC_baseURL,
   timeout: 30000,
 });
 export default axiosClient;
