18915c44da96f690d0b8f5cf2e8b3d356e8ab406
Vuong Le <vuongle.dev@gmail.com>
2024-06-24 01:01:11 +0700

    fix menu sidebar dark mode

--- src/components/header/SideMenu/MenuToggle.tsx
+++ src/components/header/SideMenu/MenuToggle.tsx
@@ -6,7 +6,7 @@ const Path = (props: any) => (
   <motion.path
     fill="transparent"
     strokeWidth="3"
-    stroke="hsl(0, 0%, 18%)"
+    stroke="hsl(var(--foreground))"
     strokeLinecap="round"
     {...props}
   />
