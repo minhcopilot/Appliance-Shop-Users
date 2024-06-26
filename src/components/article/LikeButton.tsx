661132a5c50793405f5c81c70dd5d72097ddb176
Vuong Le <vuongle.dev@gmail.com>
2024-06-26 14:17:10 +0700

    fix post view on nextjs

--- src/components/article/LikeButton.tsx
+++ src/components/article/LikeButton.tsx
@@ -3,6 +3,7 @@ import React, { use } from "react";
 import { motion } from "framer-motion";
 import { HeartFilled, HeartOutlined } from "@ant-design/icons";
 import { useLike } from "@/hooks/blog/useLike";
+import axiosClient from "@/config/axiosClient";
 
 type Props = {
   url: string;
@@ -10,7 +11,11 @@ type Props = {
 
 export default function LikeButton({ url }: Props) {
   const [like, setLike] = useLike(url)((state) => [state.like, state.setLike]);
-
+  React.useEffect(() => {
+    axiosClient.get("/article/posts/" + url + "/view", {
+      withCredentials: true,
+    });
+  }, []);
   return (
     <motion.div
       whileTap={{ scale: 0.9, filter: "drop-shadow(0 1px 1px gray)" }}
